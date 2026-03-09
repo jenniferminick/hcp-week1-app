8281fa2b1351d362a496d950036f73a82b2cbc14
jenniferminick <jennifer.minick@housecallpro.com>
2026-03-09 10:23:51 -0500

    Update tts.js

--- api/tts.js
+++ api/tts.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
         },
         body: JSON.stringify({
           text,
-          model_id: "eleven_turbo_v2",
+          model_id: "eleven_monolingual_v1",
           voice_settings: { stability: 0.4, similarity_boost: 0.8 },
         }),
       }
