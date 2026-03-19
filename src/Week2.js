ce35203987ef15c33c3d672fd854a551593bbb2d
jenniferminick <jennifer.minick@housecallpro.com>
2026-03-19 17:19:24 -0500

    fix build errors

--- src/Week2.js
+++ src/Week2.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { useState, useEffect, useRef, createContext, useContext } from "react";
 
 // ── Lang Context ──────────────────────────────────────────────────────────────
